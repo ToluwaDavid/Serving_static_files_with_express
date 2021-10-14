15c010e7b98e10b4128a2f6b87014fa020edfd36
ToluwaDavid <32324558+ToluwaDavid@users.noreply.github.com>
2021-10-14 02:03:19 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// var http = require("http");
+
+// //create a server object:
+// http
+//   .createServer(function(req, res) {
+//     res.write("Hello World!"); //write a response to the client
+//     res.end(); //end the response
+//   })
+//   .listen(8080); //the server object listens on port 8080
+var express = require("express");
+
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.listen(8080, () => {
+  console.log("Server is serving at port 8080");
+});
